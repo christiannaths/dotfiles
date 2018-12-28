d55aa775a6cb71984305c6af0bd8e751c501eeb7
Christian Naths <christiannaths@gmail.com>
2018-12-28 15:19:41 -0500

    adds global npm packages + install script

---
+++ Node/install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+HERE="$(cd "$(dirname "$0")" && pwd)"
+nodejs_exists=$(which code)
+
+if [[ -z "${nodejs_exists}" ]]; then
+    echo -e "Couldn't find nodejs. Try running 'brew install node' and make sure it succeeds before continuing. \n"
+else
+    npm install -g n
+    n lts
+    cat $HERE/packages.txt | grep -v '^#' | xargs -L1 npm install -g
+fi
--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-./Homebrew/install.sh
-./Zsh/install.sh
-./Git/install.sh
-./VSCode/install.sh
+echo -e "Installing everything \n" \
+    && ./Homebrew/install.sh \
+    && ./Zsh/install.sh \
+    && ./Git/install.sh \
+    && ./Node/install.sh \
+    && ./VSCode/install.sh \
+    && echo -e "Done. \n"
